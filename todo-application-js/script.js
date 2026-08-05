ef6aefdbe0a1b02047f89bbe4e0f2177516f5c3d
Ashutosh Mohapatra <ashutosh.mohapatra@anyhowinfo.com>
2026-08-05 12:04:36 +0530

    [Ashutosh] Todo app | Completed

--- todo-application-js/script.js
+++ todo-application-js/script.js
@@ -11,46 +11,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
   addTaskButton.addEventListener("click", () => {
     const taskText = todoInput.value.trim();
-    if (taskText === "") {
-      return;
-    }
+    if (taskText === "") return;
+
     const newTask = {
       id: Date.now(),
       text: taskText,
       completed: false,
     };
-
     tasks.push(newTask);
+    saveTasks();
+    renderTasks(newTask);
     todoInput.value = "";
-    saveTasks(tasks);
   });
-
-  function saveTasks(tasks) {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }
 
   function renderTasks(task) {
     const li = document.createElement("li");
     li.setAttribute("data-id", task.id);
-    if (task.completed) {
-      li.classList.add("completed");
-    }
+    if (task.completed) li.classList.add("completed");
     li.innerHTML = `
         <span>${task.text}</span>
         <button>Delete</button>
     `;
+
     li.addEventListener("click", (e) => {
-      if (e.target.tagName === "BUTTON") {
-        task.completed = !task.completed;
-      }
+      if (e.target.tagName === "BUTTON") return;
+      task.completed = !task.completed;
       li.classList.toggle("completed");
       saveTasks();
     });
 
     li.querySelector("button").addEventListener("click", (e) => {
-      e.stopPropagation(); // Prevent toggle from firing
-      tasks;
+      e.stopPropagation(); // Prevent toggle from firing [Event bubbling]
+      tasks = tasks.filter((t) => t.id !== task.id);
+      li.remove();
+      saveTasks();
     });
+
     todoList.appendChild(li);
+  }
+
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }
 });
