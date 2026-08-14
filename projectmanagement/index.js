e349ffd8bc1f7b351b96a2d7e6592d881407db85
Ashutosh Mohapatra <ashutosh.mohapatra@anyhowinfo.com>
2026-08-14 12:08:52 +0530

    [Ashutosh] nodemon installation | dotenv configuration

--- projectmanagement/index.js
+++ projectmanagement/index.js
@@ -1,1 +1,10 @@
-console.log("welcome string :: hello");
+import dotenv from "dotenv";
+
+// If index.js file is restructured later this will help to find where .env file is
+dotenv.config({
+  path: "./.env",
+});
+
+const myusername = process.env.username;
+
+console.log("welcome string :: hello", myusername);
