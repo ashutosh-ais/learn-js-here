eecea5d1499b18964e2053b1a0e6875f5526477e
Ashutosh Mohapatra <ashutosh.mohapatra@anyhowinfo.com>
2026-08-14 18:55:33 +0530

    [Ashutosh] API Error and response | Error class extending

--- revisit-file.js
+++ revisit-file.js
@@ -1,15 +1,23 @@
 // closure
 
-function outer() {
-  let count = 0;
-  return function inner() {
-    count = count + 1;
-    console.log(count);
-  };
-}
+// function outer() {
+//   let count = 0;
+//   return function inner() {
+//     count = count + 1;
+//     console.log(count);
+//   };
+// }
 
-let increment = outer();
+// let increment = outer();
 
-increment();
-increment();
-increment();
+// increment();
+// increment();
+// increment();
+
+const err = new Error("This is error", {
+  cause: "Only options to pass",
+});
+console.log(err.cause);
+console.log(err.message);
+console.log(err.name);
+console.log(err.stack);
