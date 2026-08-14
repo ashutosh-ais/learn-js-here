09e7ad08662dec787e43bbc92ad16f3fa13d3c69
Ashutosh Mohapatra <ashutosh.mohapatra@anyhowinfo.com>
2026-08-14 16:00:29 +0530

    [Ashutosh] Express configuration | Cors

--- projectmanagement/src/index.js
+++ projectmanagement/src/index.js
@@ -1,22 +1,12 @@
 import dotenv from "dotenv";
-// const express = require("express");
-import express from "express";
+import app from "./app.js";
 
 // If index.js file is restructured later this will help to find where .env file is
 dotenv.config({
   path: "./.env",
 });
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
-
-app.get("/instagram", (req, res) => {
-  res.send("This is an instagram page");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
