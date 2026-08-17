d04d2eb0a15326e40a737a27fc89a7f432b740da
Ashutosh Mohapatra <ashutosh.mohapatra@anyhowinfo.com>
2026-08-17 15:14:32 +0530

    [Ashutosh] Attaching pre-hook | Method to schema | Bcrypt

--- projectmanagement/src/models/user.models.js
+++ projectmanagement/src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -59,5 +60,18 @@ const userSchema = new Schema(
     timestamps: true, // created_at, updated_at
   },
 );
+
+// Attaching prehook - If password is getting modified (Hash the password and store)
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// Attaching method to the schema
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
