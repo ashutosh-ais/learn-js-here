a65a8d8521c2cb16760258bf0d6702840e8d842a
Ashutosh Mohapatra <ashutosh.mohapatra@anyhowinfo.com>
2026-08-18 19:01:43 +0530

    [Ashutosh] Auth controller | Registration In progress

---
+++ projectmanagement/src/controllers/auth.controllers.js
@@ -0,0 +1,58 @@
+import { User } from "../models/user.models";
+import ApiResponse from "../utils/api-response.js";
+import ApiError from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { emailVerificationMailgenContent, sendEmail } from "../utils/mail.js";
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating access token",
+    );
+  }
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  const existedUser = await User.findOne({
+    // Find one user where the username is ashutosh OR the email is ashutosh@gmail.com
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  const user = await User.create({
+    email,
+    password,
+    username,
+    isEmailVerified: false,
+  });
+
+  const { unHashedToken, hashedToken, tokenExpiry } =
+    user.generateTemporaryToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationExpiry = tokenExpiry;
+
+  await user.save({ validateBeforeSave: false });
+
+  await sendEmail({
+    email: user?.email,
+    subject: "Please verify your email",
+    mailgenContent: emailVerificationMailgenContent({}),
+  });
+});
